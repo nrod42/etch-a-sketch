ea7384fbfe1c004601adb7060037967fb1f2879e
nrod42 <nigelrod94@gmail.com>
2022-01-27 10:43:45 -0500

    added style buttons

--- etch.js
+++ etch.js
@@ -1,21 +1,28 @@
 const body = document.querySelector('body');
 const grid = document.querySelector('.grid');
 const clearBtn = document.querySelector('.clearBtn')
+const randomColorBtn = document.querySelector('.randomColorBtn')
+const chooseColorBtn = document.querySelector('.chooseColorBtn')
+const rainbowColorBtn = document.querySelector('.rainbowColorBtn')
+let allCells;
+gridGenerator(16);
+let color = 'black';
+chooseColor(color);
 
 function gridGenerator (gridSize) {
     //Creates a grid div of fixed size
     const gridWidth = 600;
     const gridHeight  = 600;
-    grid.style.width = gridWidth.toString() + "px";
-    grid.style.height = gridHeight.toString() + "px";
+    grid.style.width = gridWidth + "px";
+    grid.style.height = gridHeight + "px";
 
     //First loop creates the "row" divs and appends them to the grid div.
     for (let i = 0; i < gridSize; i++) {
         row = document.createElement('div');
         row.classList.add('row')
         //row dimensions are based on the fixed grid dimensions
-        row.style.width = gridWidth.toString() + "px";
-        row.style.height = (gridHeight/gridSize).toString() + "px";
+        row.style.width = gridWidth + "px";
+        row.style.height = (gridHeight/gridSize)+ "px";
         grid.appendChild(row)
         
         //Second loop creates and appends "cell" divs inside of each of the old rows. Becuase, "row"'s display is flex, these "cell"s are in a row
@@ -23,18 +30,37 @@ function gridGenerator (gridSize) {
             cell = document.createElement('div');
             cell.classList.add('cell');
             //cell dimensions are based on the fixed grid dimensions
-            cell.style.width = (gridHeight/gridSize).toString() + "px";
-            cell.style.height = (gridHeight/gridSize).toString() + "px";
+            cell.style.width = (gridHeight/gridSize) + "px";
+            cell.style.height = (gridHeight/gridSize) + "px";
+            cell.style.backgroundColor = "white";
             row.appendChild(cell);
-            //upon mouseover, each "cell" color changes
-            cell.addEventListener("mouseover", function(e) {
-                e.target.style.backgroundColor = "lightBlue";
-            })
         }
     }
+    
 }
 
-gridGenerator(16);
+function chooseColor (color) {
+    allCells = document.querySelectorAll('.cell');
+    allCells.forEach(cell => {
+        cell.addEventListener('mouseover', event => {
+            event.target.style.backgroundColor = color;
+        })
+    })
+}
+
+function randomColor () {
+    color =  '#' + Math.floor(Math.random()*16777215).toString(16);
+    return color;
+}
+
+function rainbowColor () {
+    let allCells = document.querySelectorAll('.cell');
+    allCells.forEach(cell => {
+        cell.addEventListener('mouseover', event => {
+            event.target.style.backgroundColor = randomColor();
+        })
+    })
+}
 
 //Asks user for new grid size then deletes and replaces old grid with new user grid
 function clear () {
@@ -48,7 +74,19 @@ function clear () {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-    gridGenerator(userSize)
+    gridGenerator(userSize);
+    chooseColor(color);
 }
+
+chooseColorBtn.addEventListener('click', function(e) {
+    color = prompt('Enter color:');
+    chooseColor(color);
+})
+
+randomColorBtn.addEventListener('click', function(e) {
+    chooseColor(randomColor());
+})
+
+rainbowColorBtn.addEventListener('click', rainbowColor);
 
 clearBtn.addEventListener('click', clear);
